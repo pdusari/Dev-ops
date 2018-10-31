3fbb021ef89f6f390dfad56ebee72ff301ee19ea
US\pdusari <pdusari@deloitte.com>
2018-10-31 15:13:28 +0530

    added login modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -55,3 +55,56 @@ $('#Searchprojects').on('click', function(e) {
     else $this.closest('div.logo-grid').show();
   });
 });
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function validateForm() {
+    var x = document.forms["login"]["uname"].value;
+    var atpos = x.indexOf("@deloitte");
+    var dotpos = x.lastIndexOf(".");
+
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid deloitte mail address");
+        return false;
+    }else{
+        makeid();
+    }
+
+}
+
+var loginId;
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++){
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+loginId = text;
+
+var date = new Date();
+date.setTime(date.getTime()+(24*60*60*1000));
+var expires = "; expires="+date.toGMTString();
+//var string = "token="+document.forms["login"]["uname"].value+text+expires+"; path=/";
+document.cookie = "token="+document.forms["login"]["uname"].value+text+expires;
+//console.log(string);
+//console.log("this is called"+ text);
+ShoWHideDiv();
+  return text;
+}
+
+function ShoWHideDiv() {
+    var element = document.getElementById("emailDiv");
+    element.classList.add("Hidden");
+
+    var element = document.getElementById("tokenDiv");
+    element.classList.remove("Hidden");
+}
+ShoWHideDiv()
